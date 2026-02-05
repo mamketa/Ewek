9bda871ff56dbd50108ec538ad67d06cfca4e23f
GorexTest <googlingrex@gmail.com>
2026-02-05 21:39:43 +0700

    Fix Build Jni

--- jni/Android.mk
+++ jni/Android.mk
@@ -11,7 +11,6 @@ LOCAL_SRC_FILES := \
     src/process_utils.c \
     src/misc_utils.c \
     src/preload_function.c \
-    src/dumpsys.c \
     src/mlbb_handler.c
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/include
